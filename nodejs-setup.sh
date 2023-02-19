1599200c14daa900821cf3249ea0c90dc05085bf
lavkumarsingh <lavsingh68495@gmail.com>
2023-02-19 14:19:48 +0530

    nodejs-setup

---
+++ nodejs-setup.sh
@@ -0,0 +1,15 @@
+# Refresh your local package index
+sudo apt update
+
+# Install Node.js
+sudo apt install nodejs
+
+# Install npm
+sudo apt install npm
+
+# Check version
+echo "Node version"
+node -v
+
+echo "NPM version"
+npm -v
