f78de10f86dd145a48313592c61d9e029db8e015
lavkumarsingh <lavsingh68495@gmail.com>
2023-02-19 23:28:22 +0530

    nvm install command added

--- nodejs-setup.sh
+++ nodejs-setup.sh
@@ -7,9 +7,13 @@ sudo apt install nodejs
 # Install npm
 sudo apt install npm
 
+# Install npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
+
 # Check version
 echo "Node version"
 node -v
-
 echo "NPM version"
 npm -v
